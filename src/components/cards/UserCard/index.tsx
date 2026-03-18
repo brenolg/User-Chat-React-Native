f23ebae4532aaca30001b277cf6f882f2322a5e4
brenolg <brenolgar@gmail.com>
2026-03-18 11:58:46 -0300

    chore: comentarios em usercard

--- src/components/cards/UserCard/index.tsx
+++ src/components/cards/UserCard/index.tsx
@@ -13,13 +13,17 @@ type NavigationProps = NativeStackNavigationProp<
 >;
 
 type Props = {
-  user: User;
+  user: User; // dados do usuário
   onPress: () => void;
 };
 
 function UserCard({ user, onPress }: Props) {
   const navigation = useNavigation<NavigationProps>();
 
+  /**
+   * Navega diretamente para a tela de chat com o usuário selecionado
+   * useCallback evita recriação desnecessária da função
+   */
   const handleGoToChat = useCallback(() => {
     navigation.navigate("ChatDetails", {
       selectedUser: user,
@@ -27,12 +31,15 @@ function UserCard({ user, onPress }: Props) {
   }, [navigation, user]);
 
   return (
+    // Wrapper clicável do card (leva para perfil)
     <TouchableOpacity onPress={onPress}>
       <Container>
+        {/* Avatar do usuário */}
         {user.picture?.thumbnail && (
           <Avatar source={{ uri: user.picture.thumbnail }} />
         )}
 
+        {/* Informações do usuário */}
         <Info>
           <Name numberOfLines={1} ellipsizeMode="tail">
             {user.name.first} {user.name.last}
@@ -43,6 +50,7 @@ function UserCard({ user, onPress }: Props) {
           </Email>
         </Info>
 
+        {/* Botão para ir direto ao chat */}
         <ChatButton onPress={handleGoToChat}>
           <Ionicons name="chatbubbles-outline" size={25} color="#fff" />
         </ChatButton>
@@ -51,6 +59,10 @@ function UserCard({ user, onPress }: Props) {
   );
 }
 
+/**
+ * Memoização do componente para evitar re-renderizações desnecessárias
+ * Só re-renderiza se o UUID do usuário mudar
+ */
 export default React.memo(UserCard, (prev, next) => {
   return prev.user.login.uuid === next.user.login.uuid;
 });
